ea97fddb72621d8a95d2c398041a98b9b2fb653e
alexandre-b-andrade <46202675+alexandre-b-andrade@users.noreply.github.com>
2020-04-05 18:44:18 -0300

    Delete _input_config.py

--- Windows py scripts/_input_config.py
+++
@@ -1,5 +0,0 @@
-def config():
-    inputfile = "\\Inputs\\2019_11_08_INPUT_MIP_MODEL_73_TASKS_cmax aumentado.xlsx" # For Windows use
-    #inputfile = "/Inputs/2019_11_08_INPUT_MIP_MODEL_10_TASKS.xlsx" # For Linux use
-    time_limit = 24 # Hours
-    return (inputfile, time_limit)
